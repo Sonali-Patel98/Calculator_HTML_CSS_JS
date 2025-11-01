bb20b1c4637bd6d8ed0310c6a6951fc71396859c
Sonali-Patel98 <sonalipatel9845@gmail.com>
2025-11-01 13:56:35 +0530

    updated

---
+++ app.js
@@ -0,0 +1,24 @@
+let inp=document.getElementById("con-text");   //return like array byclassName
+let buttons=document.querySelectorAll("button"); //return satic nodelist
+let str= "";
+let arr=Array.from(buttons);
+arr.forEach(btn=>{
+    btn.addEventListener("click",()=>{
+       const value=btn.textContent;
+       if(value == "Ac"){
+        str= "";
+       }
+       else if(value== "DEL"){
+        str=str.slice(0,-1);
+       }
+       else if(value== "="){
+        str=eval(str);
+        str="="+" " +str;
+       }
+       else{
+        str+=value;
+       }
+       inp.value=str;
+    
+    });
+});
